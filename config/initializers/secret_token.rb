0a41d4a8c0b452db5a4709c041def7f459434ce6
Michel Belleville <mib@octo.com>
2014-04-08 16:35:07 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CityGrids::Application.config.secret_key_base = '5fe67090b9805cdafb1ea760dd01b315d68a469427c69d8151c07e046489ede531ac7a1408a1c60e0d5ecf6b784a9d970a8714a4add4db84287e4fc738b8d6aa'
