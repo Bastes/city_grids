f6acd82b81886f9e01b4a59185857b6431959fcb
Michel Belleville <mib@octo.com>
2014-04-10 17:11:28 +0200

    View improvements (style, specs, etc.)

--- spec/views/cities/index.html.slim_spec.rb
+++ spec/views/cities/index.html.slim_spec.rb
@@ -6,10 +6,14 @@ describe 'cities/index.html.slim' do
 
   before { render }
 
+  subject { rendered }
+
+  it { should_not have_selector(%Q(.translation_missing)) }
+
   specify 'the cities list' do
-    within 'ul#cities' do |list|
+    within '#cities ul' do |list|
       cities.each_with_index do |city, i|
-        list.should have_selector %Q(li:nth-child(#{i + 1}) a[href="#{view.city_path(city)}"]), text: city.name
+        list.should have_selector %Q(li:nth-child(#{i + 1}) h2 a[href="#{view.city_path(city)}"]), text: city.name
       end
     end
   end
--- spec/views/cities/show.html.slim_spec.rb
+++ spec/views/cities/show.html.slim_spec.rb
@@ -10,16 +10,18 @@ describe 'cities/show.html.slim' do
 
   subject { rendered }
 
+  it { should_not have_selector(%Q(.translation_missing)) }
+
   describe 'the city' do
-    it { should have_selector(%Q(#city .city), text: city.name) }
+    it { should have_selector(%Q(#city .city h2), text: city.name) }
   end
 
   specify 'incoming tournaments' do
-    within '#city ul#tournaments' do |list|
+    within '#city .tournaments ul' do |list|
       incoming_tournaments.each_with_index do |tournament, i|
         within list, %Q(li:nth-child(#{i + 1})) do |item|
-          item.should have_selector %Q(.name), text: tournament.name
-          item.should have_selector %Q(.begins_on), text: I18n.l(tournament.begins_at.to_date, format: :long)
+          item.should have_selector %Q(h4 .name), text: tournament.name
+          item.should have_selector %Q(h4 .begins-on), text: I18n.l(tournament.begins_at.to_date, format: :long)
           item.should have_selector %Q(a.address[href="#{tournament.address_url}"][target="_blank"]), text: tournament.address
           item.should have_selector %Q(.places), text: %r(\b#{tournament.places}\b)
         end
