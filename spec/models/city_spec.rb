60dfe54801a8b40340a65520be9864a9d590982d
Michel Belleville <mib@octo.com>
2014-04-11 12:05:17 +0200

    Incoming tournaments might as well be ordered

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -6,11 +6,11 @@ describe City do
 
     describe '#incoming_tournaments' do
       subject(:city) { create :city }
-      let!(:incoming_tournaments) { 3.times.map { |n| create :tournament, begins_at: n.days.from_now, city: city } }
+      let!(:incoming_tournaments) { [2, 7, 1].map { |n| create :tournament, begins_at: n.days.from_now, city: city } }
       before { 2.times { |n| create :tournament, begins_at: (n + 1).days.ago, city: city } }
       before { 2.times { |n| create :tournament, begins_at: n.days.from_now, city: create(:city) } }
 
-      its(:incoming_tournaments) { should match_array incoming_tournaments }
+      its(:incoming_tournaments) { should eq incoming_tournaments.sort_by(&:begins_at) }
     end
   end
 
