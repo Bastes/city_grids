d7570a75fffd401b9fe85575e349995411d6b084
Michel Belleville <mib@octo.com>
2014-04-20 16:21:23 +0200

    Displaying the present tickets count on the tournaments list

--- spec/views/cities/show.html.slim_spec.rb
+++ spec/views/cities/show.html.slim_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe 'cities/show.html.slim' do
   let(:city) { create :city }
-  before { create_list(:tournament, 3, city: city).map &:decorate }
+  let!(:tournaments) { create_list(:tournament, 3, city: city) }
+  let!(:tickets) { tournaments.inject([]) { |r, tournament| r + create_list(:ticket, rand(1..5), tournament: tournament) } }
   before { assign :city, city.reload }
 
   before { expect(view).to receive(:current_city).with(city) }
@@ -25,9 +26,23 @@ describe 'cities/show.html.slim' do
             link.should have_selector %Q(.begins-on), text: I18n.l(tournament.begins_at.to_date, format: :long)
           end
           item.should have_selector %Q(a.address[href="#{tournament.address_url}"][target="_blank"]), text: tournament.address
-          item.should have_selector %Q(.places), text: %r(\b#{tournament.places}\b)
+          item.should have_selector %Q(.places .all),   text: I18n.t('cities.show.places.all', count: tournament.places)
+          item.should have_selector %Q(.places .taken), text: I18n.t('cities.show.places.taken', count: tournament.tickets.present.count)
         end
       end
     end
+  end
+
+  context 'a tournament has no places' do
+    let!(:tournaments) { create_list(:tournament, 1, city: city, places: nil) }
+
+    it { should_not have_selector %Q(#city .tournaments ul li .places .all) }
+  end
+
+  context 'a tournament has no places taken' do
+    let!(:tournaments) { create_list(:tournament, 1, city: city) }
+    let!(:tickets) { [] }
+
+    it { should_not have_selector %Q(#city .tournaments ul li .places .taken) }
   end
 end
